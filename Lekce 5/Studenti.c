a81515a8b34ad00f003e130cfd16eb25bddda976
Petr-Pan <panek.pet@seznam.cz>
2017-11-04 18:00:22 +0100

    Přidány funkce a první pokusy o jejich debug.
    Funkce na porovnání věku a tisk výsledku porovnání.

--- Lekce 5/Studenti.c
+++ Lekce 5/Studenti.c
@@ -1,12 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-#Zadání - porovnání dvou věků studentů funkcí přes vlastní strukturované datové typy
-#  http://jazykc.inf.upol.cz/strukturovane-datove-typy/studenti.htm
-
 typedef struct {
 	char Den, Mesic; short Rok;
 } Datum;
+
 
 
 typedef struct {
@@ -14,18 +12,71 @@ typedef struct {
 } student;
 
 int porovnej_vek(student s1, student s2) {
+	/* Pokud je s1 starší, vrátí hodnotu 1. Pokud je s2 starší, vrátí hodnotu -1.
+	U shodného věku vrátí hodnotu 0.*/
+	if (s1.narozen.Rok < s2.narozen.Rok)
+		return 1;
 
+	else if (s1.narozen.Rok == s2.narozen.Rok)
+		if (s1.narozen.Mesic < s2.narozen.Mesic)
+			return 1;
+		else if (s1.narozen.Mesic == s2.narozen.Mesic)
+			if (s1.narozen.Den < s2.narozen.Den)
+				return 1;
+			else if (s1.narozen.Den == s2.narozen.Den)
+				return 0;
+			else
+				return -1;		
+		else
+			return -1;
+	else
+		return -1;
 
 };
 
-int main() {
-	student s1;
-	s1.jmeno = "Erich";
-	s1.prijmeni = "Fiedler";
-	s1.narozen = {11, 11, 1989};
+int porovnanyvek = 0;
+
+int printstarsiho(student s1, student s2, int porovnanyvek) {
+	/* Vytiskne porovnání věků dvou studentů.*/
+	switch (porovnanyvek) {
+		case 1:
+			printf("%c", s1.jmeno, "%c", s1.prijmeni, "je starsi než %c", s2.jmeno, "%c", s2.prijmeni, ".\n");
+			break;
+		case -1:
+			printf("%c", s1.jmeno, "%c", s1.prijmeni, "je mladsi než %c", s2.jmeno, "%c", s2.prijmeni, ".\n");
+			break;
+		case 0:
+			printf("%c", s1.jmeno, "%c", s1.prijmeni, "je stejne stary jako %c", s2.jmeno, "%c", s2.prijmeni, ".\n");
+			break;
+	}
+	return 0;
+};
 	
+
+int main() {
+	student s3;
+	s3.jmeno = "Erich";
+	s3.prijmeni = "Fiedler";
+	s3.narozen.Den = 11;
+	
+	student s1 = { "Doktor", "Zaba", {7, 7, 1980} };
 	student s2 = { "Kuzma", "Kuzmic", {11, 12, 1980} };
 
+	//printstarsiho(s1, s2, porovnej_vek(s1, s2));
+	
+	int porovnani = porovnej_vek(s1, s1);
+	
+	printstarsiho(s1, s2, porovnani);
+
+	//Všechno směrem dolů pokusy o debug
+
+	char *s2j = s2.jmeno;
+
+
+	printf("%s", *s2j);
+	printf("%d", porovnani);
+	printf("%d", s1.narozen.Den);
+	
 
 
 
